ca218f612913f9c9ab0e36f5e5548e027790b66a
Mathieu BRUNERIE <mb936701@iut21.u-bourgogne.fr>
2023-10-25 17:13:13 +0200

    question 3.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { BreadCrumbs, SectionContainer } from "tp-kit/components";
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../components/product-list";
 import { Metadata } from "next";
-import prisma from "../utils/prisma";
+import {getCategories} from "../utils/get-category";
 
 export const metadata:Metadata = {
   title: `Page d’accueil - Starbucks`,
   description: "Commandez de délicieuses boissons préparées avec soin par nos baristas"
 }
 
-const categories = await prisma.productCategory.findMany({ include: { products: true } });
+const categories = await getCategories();
 
 export default async function Home() {
   return (<SectionContainer>
